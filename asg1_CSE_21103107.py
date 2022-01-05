3097e6e63369da0946bad91e32dc9060c5e34881
mishank2002 <96677559+mishank2002@users.noreply.github.com>
2022-01-05 21:51:52 +0530

    Update asg1_CSE_21103107.py

--- asg1_CSE_21103107.py
+++ asg1_CSE_21103107.py
@@ -1,6 +1,6 @@
 # Question 1
 a=input("Enter first number")
-b=input("Enyter second number")
+b=input("Enter second number")
 c=input("Enter third number")
 d=(int(a)+int(b)+int(c))/3
 print("The average of three numbers is :")
