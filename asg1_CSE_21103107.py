0564d2959a9959c591446ecd084c547995874079
mishank2002 <96677559+mishank2002@users.noreply.github.com>
2022-01-08 16:53:57 +0530

    Update asg1_CSE_21103107.py

--- asg1_CSE_21103107.py
+++ asg1_CSE_21103107.py
@@ -13,7 +13,7 @@ standard_deduction=10000
 depend_deduction=3000
 gross=input("enter your gross income")
 No_of_dependents=input("Enter your number of dependents")
-taxable_income=int(gross)-int(standard_deduction)-(int(depend_deduction)+int(No_of_dependents))
+taxable_income=int(gross)-int(standard_deduction)-(int(depend_deduction)*int(No_of_dependents))
 tax=(float(taxable_income)*0.2)
 print("Your income tax is :")
 print(float(tax))
