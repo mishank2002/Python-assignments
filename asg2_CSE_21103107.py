8dcae5ecd1568345c2f2e8d4d3bd838390eb6f2d
mishank2002 <96677559+mishank2002@users.noreply.github.com>
2022-01-17 15:15:32 +0530

    Add files via upload

--- asg2_CSE_21103107.py
+++ asg2_CSE_21103107.py
@@ -1,73 +1,44 @@
-# QUESTION 1
-string1="Python is a case sensitive language" #string1
-print("<A>THE LENGTH OF STRING IS",len(string1)) #function to find LENGTH OF STRING
-print("<B>THE REVERSED STRING IS ",string1[-1::-1])
-string2=string1[10:26] #STORED a case sensitive IN A NEW STRING
-string2="Object Oriented" #REPLACED "a csae sensitive" WITH "object oriented"
-print("<E>THE INDEX OF SUBSTRING a is ",string1.find('a'))
-print("<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS",string1.replace(" ",""))
-print("\n")
+{\rtf1\ansi\ansicpg1252\cocoartf2578
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
-
-#QUESTION 2
-NAME=input("ENTER YOUR NAME")
-SID=input("ENTER YOUR SID")
-DEPARTMENT=input("ENTER YOUR DEPARTMENT")
-CGPA=input("ENTER YOUR CGPA")
-print("Hey,",NAME,"Here!")
-print("My SID is ",SID)
-print("I am from ",DEPARTMENT,"and my CGPA is ",CGPA)
-print("\n")
-
-
-#QUESTION 3
-a=56
-b=10
-print("a. ",a&b)
-print("b. ",a|b)
-print("c. ",a^b)
-print("Left shift both a and b with 2 bits respectively are :",a<<2 ,b<<2)
-print("Right shift a with 2 bits and b with 4 bits respectively are : ",a>>2,b>>4)
-print("\n")
-
-
-#QUESTION 4
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
- 
-if (num1 > num2) and (num1 > num3):
-   largest = num1
-elif (num2 > num1) and (num2 > num3):
-   largest = num2
-else:
-   largest = num3
- 
-print("THE LARGEST NUMBER IS ",largest)
-print("\n")
-
-
-#QUESTION 5
-s=input("ENTER A STRING")
-if (s.find('name') != -1):
-    print ("Yes")
-else:
-    print ("No")
-print("\n")
-
-#QUESTION 6
-a=int(input("ENTER FIRST SIDE OF TRIANGLE"))
-b=int(input("ENTER SECOND SIDE OF TRIANGLE"))
-c=int(input("ENTER THIRD SIDE OF TRIANGLE"))
-if(a>(b+c)):
-    print("No")
-elif(b>(a+c)):
-    print("No")
-elif(c>(a+b)):
-    print("No")
-else:
-    print("Yes")
-print("\n")
-
-    
-
+\f0\fs24 \cf0 \
+mishankkumar@Mishanks-MacBook-Air python assignments % alias python=python3\
+mishankkumar@Mishanks-MacBook-Air python assignments % python -u "/Users/mishankkumar/Desktop/python assignments/asg2_CSE_21103107.py"\
+<A>THE LENGTH OF STRING IS 35\
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP\
+<E>THE INDEX OF SUBSTRING a is  10\
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage\
+\
+\
+ENTER YOUR NAME MISHANK\
+ENTER YOUR SID 21103107\
+ENTER YOUR DEPARTMENT CSE\
+ENTER YOUR CGPA 9.5\
+Hey,  MISHANK Here!\
+My SID is   21103107\
+I am from   CSE and my CGPA is   9.5\
+\
+\
+a.  8\
+b.  58\
+c.  50\
+Left shift both a and b with 2 bits respectively are : 224 40\
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0\
+\
+\
+Enter first number: 32\
+Enter second number: 43\
+Enter third number: 69\
+THE LARGEST NUMBER IS  69\
+\
+\
+ENTER A STRING MISHANK\
+No\
+ENTER FIRST SIDE OF TRIANGLE 34\
+ENTER SECOND SIDE OF TRIANGLE 54\
+ENTER THIRD SIDE OF TRIANGLE 84\
+Yes}
