e929feb65b957c1549d6d624fd78562d2fc30051
mishank2002 <96677559+mishank2002@users.noreply.github.com>
2022-01-18 16:49:55 +0530

    Update asg2_CSE_21103107.py

--- asg2_CSE_21103107.py
+++ asg2_CSE_21103107.py
@@ -3,7 +3,7 @@ string1="Python is a case sensitive language" #string1
 print("<A>THE LENGTH OF STRING IS",len(string1)) #function to find LENGTH OF STRING
 print("<B>THE REVERSED STRING IS ",string1[-1::-1])
 string2=string1[10:26] #STORED a case sensitive IN A NEW STRING
-string2="Object Oriented" #REPLACED "a csae sensitive" WITH "object oriented"
+string2.replace("a case sensitive","object oriented") #REPLACED "a case sensitive" WITH "object oriented"
 print("<E>THE INDEX OF SUBSTRING a is ",string1.find('a'))
 print("<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS",string1.replace(" ",""))
 print("\n")
