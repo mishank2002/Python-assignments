fbad4a179ac67ddc2f3b31936dd80deee6b449b9
mishank2002 <96677559+mishank2002@users.noreply.github.com>
2022-01-23 10:41:39 +0530

    Update asg2_output_21103107.py

--- asg2_output_21103107.py
+++ asg2_output_21103107.py
@@ -1,43 +1,38 @@
-mishankkumar@Mishanks-MacBook-Air python assignments % alias python=python3\
-mishankkumar@Mishanks-MacBook-Air python assignments % python -u "/Users/mishankkumar/Desktop/python assignments/asg2_CSE_21103107.py"\
-#Question 1
-<A>THE LENGTH OF STRING IS 35\
-<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP\
-<E>THE INDEX OF SUBSTRING a is  10\
-<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage\
-\
-\
-#Question 2
-ENTER YOUR NAME MISHANK\
-ENTER YOUR SID 21103107\
-ENTER YOUR DEPARTMENT CSE\
-ENTER YOUR CGPA 9.5\
-Hey,  MISHANK Here!\
-My SID is   21103107\
-I am from   CSE and my CGPA is   9.5\
-\
-\
-#Question 3
-a.  8\
-b.  58\
-c.  50\
-Left shift both a and b with 2 bits respectively are : 224 40\
-Right shift a with 2 bits and b with 4 bits respectively are :  14 0\
-\
-\
-#Question 4    
-Enter first number: 32\
-Enter second number: 43\
-Enter third number: 69\
-THE LARGEST NUMBER IS  69\
-\
-\
-#Question 5
-ENTER A STRING MISHANK\
-No\
+mishankkumar@Mishanks-MacBook-Air python assignments % python -u "/Users/mishankkumar/Desktop/python assignments/practice.py"
+<A>THE LENGTH OF STRING IS 35
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+<E>THE INDEX OF SUBSTRING a is  10
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
 
-#Question 6
-ENTER FIRST SIDE OF TRIANGLE 34\
-ENTER SECOND SIDE OF TRIANGLE 54\
-ENTER THIRD SIDE OF TRIANGLE 84\
-Yes}
+
+ENTER YOUR NAME MISHANK KUMAR
+ENTER YOUR SID 21103107
+ENTER YOUR DEPARTMENT CSE
+ENTER YOUR CGPA 9.5
+Hey  MISHANK KUMAR, Here!
+My SID is 21103107
+I am from  CSE and my CGPA is 9.500000
+
+
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+
+Enter first number: 4
+Enter second number: 5
+Enter third number: 6
+THE LARGEST NUMBER IS  6
+
+
+ENTER A STRING MISHANK
+No
+
+
+ENTER FIRST SIDE OF TRIANGLE 4
+ENTER SECOND SIDE OF TRIANGLE 5
+ENTER THIRD SIDE OF TRIANGLE 6
+Yes
+mishankkumar@Mishanks-MacBook-Air python assignments % 
