904acdaf9a7004a075a6ce0435276586c07c8fd0
mishank2002 <96677559+mishank2002@users.noreply.github.com>
2022-02-15 13:27:27 +0530

    Update asg3_CSE_21103107.py

--- asg3_CSE_21103107.py
+++ asg3_CSE_21103107.py
@@ -1,8 +1,8 @@
 #QUESTION1
 print("QUESTION 1")
 a=str(input("ENTER ANY STRING: "))
-list=a.split() #To split all the elements of string in a list
 a=a.lower() #to convert string into lower case
+list=a.split() #To split all the elements of string in a list
 dict={} #initializing an empty dictionary
 if list.__len__()==1:   #if statement will be implemented when a single word is entered
     for i in list[0]:
