51a6aa26264a3d6be555af9f420906d51308e50d
mishank2002 <96677559+mishank2002@users.noreply.github.com>
2022-02-23 16:53:11 +0530

    Update asg4_CSE_21103107.py

--- asg4_CSE_21103107.py
+++ asg4_CSE_21103107.py
@@ -13,7 +13,11 @@ print("\n")
 #QUESTION 2
 print("ANSWER 2")
 from math import factorial, remainder
+from tracemalloc import start
 n=int(input('Enter the size of pascals triangle '))
+
+print("USING LOOPS")
+
 for i in range(n):
 	for j in range(n-i+1):
 		print(end=" ") #for spacing
@@ -21,7 +25,31 @@ for i in range(n):
 	for j in range(i+1):
 		print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")	# nCr = n!/((n-r)!*r!)
 	print()	# for new line
+print("\n\n")
+
+print("USING RECURSSIONS")
+
+def pascal_triangle(n,originalength=n):
+    if n == 0:
+        return
+    pascal_triangle(n-1,originalength)
+    #printing the spaces
+    print('  '*(originalength-n), end='')
+
+    #first number is always 1
+    start = 1
+    for i in range(1, n+1):
+
+        print(start, end ='   ')
+        
+        #using Binomial Coefficient
+        start = start * (n - i) // i
+    print()
+pascal_triangle(n)
 print("\n")
+
+
+
 
 #QUESTION 3
 print("ANSWER 3")
