ab2a41c778f9fbbf7421a96279cf0c9915e13d3e
mishank2002 <96677559+mishank2002@users.noreply.github.com>
2022-02-23 16:37:53 +0530

    Add files via upload

---
+++ asg4_output_21103107.py
@@ -0,0 +1,51 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================================================= RESTART: /Users/mishankkumar/Desktop/coding/Python assignment/asg4_CSE_21103107.py =========================================================
+ANSWER 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+ANSWER 2
+Enter the size of pascals triangle 6
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+ANSWER 3
+Enter 2 numbers: 10 3
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [7, 5, 6, 8, 6, 9, 7]
+<d> Set: {5, 6, 7, 8, 9}
+<e> Immutable set: frozenset({5, 6, 7, 8, 9})
+<f> Hash value of the max value from the above set: 9
+
+
+ANSWER 4 
+Object created
+The name of the student it Mishank and SID is 21103107.
+Object destroyed
+
+
+ANSWER 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+ANSWER 6
+Enter the word: lead
+Enter a new meaningful word using the exact same letters to test your friendship: deal
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
